1e05d917289fb724e3d4e25e3d77ca2572880123
amxra <amiradediran@gmail.com>
2019-08-23 09:11:57 +0100

    Complete request 1 to 3 object-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,42 +12,50 @@
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
-function Dinosaur (name,diet,weight,length, period){
-  this.name =name;
-  this.diet = diet;
-  this.weight = weight;
-  this.length = length;
-  this.period = period;
+
+const tyrannosaurus = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight:"7000kg",
+  length: "12m", 
+  period: "Late Cretaceous",
 }
 
-const tyrannosaurus = new Dinosaur("tyrannosaurus","carnivorous", "7000kg", "12m", "Late Cretaceous" );
-const velociraptor = new Dinosaur ("velociraptor", "carnivorous", "15kg", "1.8m", "Late Cretaceous" );
-const stegosaurus = new Dinosaur("stegosaurus", "herbivorous", "2000kg", "9m", "Late Jurassic")
+const stegosaurus ={
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight:"2000kg",
+  length: "9m", 
+  period: "Late Jurassic"
+}
 
-
+const velociraptor ={
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight:"15kg",
+  length: "1.8m", 
+  period: "Late Cretaceous"
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
-// // How much did tyrannosaurus weigh?
+// How much did tyrannosaurus weigh?
 console.log(tyrannosaurus.weight);
 
-// // What was the diet of a velociraptor?
-console.log(velociraptor.diet)
+// What was the diet of a velociraptor?
+console.log(velociraptor.diet);
 
-// // How long was a stegosaurus?
+// How long was a stegosaurus?
 console.log(stegosaurus.length);
 
-// // What time period did tyrannosaurus live in?
+// What time period did tyrannosaurus live in?
 console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 
-// tyrannosaurus.prototype.speak = function speak (){
-//    return 'RAWERSRARARWERSARARARRRR!'
-// }
-
-// console.log();
+tyrannosaurus.sound = () => "AWERSRARARWERSARARARRRR!"
+console.log(tyrannosaurus.sound());
 
 
 // ==== Arrays ====
@@ -66,7 +74,6 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  */
-
 function extractUni (person){
   return person.university
 }
@@ -75,6 +82,7 @@ function extractUni (person){
 let universities = [];
 
 universities= graduates.map(extractUni);
+
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -83,12 +91,23 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+let contactInfo = [];
+
+contactInfo = graduates.map((graduate)=>{
+  return graduate.first_name + '' + graduate.email;
+})
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+
+let uni = [];
+
+uni = uni.filter((university) => {
+  return university.includes("Uni");
+})
+
 console.log(uni);
 
 
